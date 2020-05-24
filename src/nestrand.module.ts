8d5b29a9018a18c45b2fe83210347c4ea4d8a67a
Paras Mendiratta <paras@ezibyte.com>
2020-05-24 06:12:31 +0530

    sample library done

---
+++ src/nestrand.module.ts
@@ -0,0 +1,24 @@
+import { DynamicModule, Module } from "@nestjs/common";
+import { RandomNumberServiceOptions } from "./random-number-service-options";
+import { RandomNumberService } from "./random-number.service";
+import { TelegramModule } from './telegram/telegram.module';
+
+@Module({
+  imports: [TelegramModule]
+})
+export class NestrandModule {
+  static forRoot(options: Partial<RandomNumberServiceOptions>): DynamicModule {
+    const providers = [
+      {
+        provide: RandomNumberService,
+        useValue: new RandomNumberService(options),
+      },
+    ];
+
+    return {
+      providers: providers,
+      exports: providers,
+      module: NestrandModule,
+    };
+  }
+}
---
+++ src/random-number.service.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { RandomNumberServiceOptions } from "./random-number-service-options";
+import { RandomNumberService } from "./random-number.service";
+
+describe("RandomNumberService", () => {
+  let service: RandomNumberService;
+  const options: Partial<RandomNumberServiceOptions> = {};
+
+  const providers = [
+    {
+      provide: RandomNumberService,
+      useValue: new RandomNumberService(options),
+    },
+  ];
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: providers,
+    }).compile();
+
+    service = module.get<RandomNumberService>(RandomNumberService);
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  it("#1", () => {
+    expect(service.generate()).toBeDefined();
+  });
+});
